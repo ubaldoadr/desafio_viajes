9b0ad041b1db3f9acebab66444989b05011569af
ubaldoadr <evonymanunache@gmail.com>
2024-03-27 22:57:03 -0300

    creacion seccion footer y detalles

---
+++ assets/js/viajes.js
@@ -0,0 +1,17 @@
+$("#enviar-correo").click(function () {
+    alert("El correo fue enviado correctamente...");
+  });
+  
+  $(document).ready(function () {
+    $("#destacados").on("dblclick", function () {
+      $("#quienes").css("color", "yellow");
+    });
+  });
+   
+  $(document).ready(function () {
+      $("#destacados").on("dblclick", function () {
+        $("#quienes").css("color", "yellow");
+      });
+    });
+  
+   
